b5e683444f8fcafe3aec818a8d6d85de12c74e4e
Sergio Shevchenko <shevchenkser@gmail.com>
2019-01-14 22:55:33 +0100

    WIP: bitwriter mask selection

--- osdna_bitwriter.cpp
+++ osdna_bitwriter.cpp
@@ -2,8 +2,53 @@
 #include <cstdlib>
 #include "osdna_bitwriter.h"
 
+char get_mask_for_rotation(int i) {
+    char mask = 0xFF;
+    switch (i) {
+        case 0: {
+            mask = 0xC0;
+            break;
+        }
+        case 1: {
+            mask = 0x30;
+            break;
+        }
+        case 2: {
+            mask = 0x0C;
+            break;
+        }
+        case 3: {
+            mask = 0x03;
+            break;
+        }
+        default: {
+            printf("get_mask_rotation unexpected rotation value");
+            mask = 0x03;
+        }
+    }
+
+    return mask;
+}
+
+char get_char_by_mask(char bb, int i) {
+    char section = (bb & get_mask_for_rotation(i));
+    section = section << 2 * i;
+    if (section == 0x00)
+        return 'A';
+    if (section == 0x40)
+        return 'C';
+    if (section == 0x80)
+        return 'G';
+    if (section == 0xC0)
+        return 'T';
+    return '?';
+}
+
 osdna_bit_handler *osdna_bit_init(FILE *write_stream) {
     osdna_bit_handler *ctx = (osdna_bit_handler *) malloc(sizeof(osdna_bit_handler));
     return ctx;
 }
 
+osdna_error write_char(osdna_bit_handler *handle, char c) {
+
+}
--- osdna_bitwriter.h
+++ osdna_bitwriter.h
@@ -1,15 +1,16 @@
 #ifndef OSDNA_OSDNA_BITWRITER_H
 #define OSDNA_OSDNA_BITWRITER_H
 
+#include "osdna_compressor.h"
 struct osdna_bit_handler {
     FILE *write_stream = NULL;
     char write_buffer[1024];
     char current_window = 0x00;
-    short byte_position = 0;
+    short bit_position = 0;
 };
 
 osdna_bit_handler * osdna_bit_init(FILE *write_stream);
-
+osdna_error write_char(osdna_bit_handler* handle, char c);
 
 
 #endif //OSDNA_OSDNA_BITWRITER_H
