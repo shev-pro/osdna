775077b658b9f39bb5bb40a269274d9b11f4c08d
ALI-PC\ali <lino.sarto@gmail.com>
2019-02-01 11:31:23 +0100

    opt trigger and bit enc function

--- osdna_compression_core.cpp
+++ osdna_compression_core.cpp
@@ -2,10 +2,12 @@
 #include "osdna_utils.h"
 #include "osdna_bitwriter.h"
 #include "osdna_bitreader.h"
+#include <string.h>
+#include <math.h>
 
-#define BIT_ADVANTAGE 0
-#define FILE_SIZE 1
-#define MAX_TRIGGER_SIZE 1024*2
+#define BUFF_SIZE 1024
+#define MAX_TRIGGER_SIZE 2000
+#define BIT_ENCODE_SIZE 10
 #define POS(X) (X == 'A' ? 0 : (X=='C' ? 1 : (X=='G' ? 2 : (X=='T' ? 3 : -1))))
 
 
@@ -79,16 +81,20 @@ osdna_error compress_core(OSDNA_ctx *ctx) {
 osdna_error opt_trigger_calc(FILE *read_stream, int *trigger_size, int *bit_encoding) {
     *trigger_size = 0;
     *bit_encoding = 0;
-    long occ_matr[4][MAX_TRIGGER_SIZE] = {{0}};
-    int bit_advantage[2048][MAX_TRIGGER_SIZE] = {{0}};
-    int trigger_size_advantage[2048][MAX_TRIGGER_SIZE][2] = {{0}};
-    unsigned long total_byte = 0;
-    char buff[102400];
+    unsigned long occ_matr[4][MAX_TRIGGER_SIZE];
+    long long bit_advantage[BIT_ENCODE_SIZE][MAX_TRIGGER_SIZE];
+    long long trigger_size_advantage[BIT_ENCODE_SIZE][MAX_TRIGGER_SIZE];
+    unsigned long long total = 0;
+    char buff[BUFF_SIZE];
     char last_char = 0, curr_char;
-    int bytesRead, count = 1, opt_trigger_size = 0;
+    int bytesRead, cont = 1;
+
+    memset(occ_matr, 0, 4 * MAX_TRIGGER_SIZE * sizeof(unsigned long));
+    memset(bit_advantage, 0, BIT_ENCODE_SIZE * MAX_TRIGGER_SIZE * sizeof(long long));
+    memset(trigger_size_advantage, 0, BIT_ENCODE_SIZE * MAX_TRIGGER_SIZE * sizeof(long long));
 
     if (read_stream == (FILE *) NULL) {
-        printf("Error opening file\n");
+        printf("Error opening file %s\n", read_stream);
         return OSDNA_IO_ERROR;
     }
 
@@ -97,80 +103,74 @@ osdna_error opt_trigger_calc(FILE *read_stream, int *trigger_size, int *bit_enco
         printf("Error empty file\n");
         return OSDNA_IO_ERROR;
     }
-    total_byte++;
+    total++;
     last_char = buff[0];
-    while (bytesRead = fread(buff, 1, 102400, read_stream)) {
+    while (bytesRead = fread(buff, 1, BUFF_SIZE, read_stream)) {
         for (int i = 0; i < bytesRead; i++) {
             curr_char = buff[i];
-            total_byte++;
+            total++;
             if (POS(curr_char) == -1) {
                 printf("Bad file\n");
-                return OSDNA_DATA_CHAR_ERROR;
+                //return -1;
+                continue;  //skip bad character occurrences
             }
             if (curr_char == last_char)
-                count++;
+                cont++;
             else {
-                if (count > MAX_TRIGGER_SIZE) {
+                if (cont > MAX_TRIGGER_SIZE) {
                     printf("Too long trigger size \n");
-                    return OSDNA_CONFIG_ERROR;
+                    return OSDNA_IO_ERROR;
                 }
-                occ_matr[POS(last_char)][count]++;
-                count = 1;
+                occ_matr[POS(last_char)][cont]++;
+                cont = 1;
                 last_char = curr_char;
             }
         }
     }
-    occ_matr[POS(last_char)][count]++;
+    occ_matr[POS(last_char)][cont]++;
 
-    int modulo = 3;
-    for (int l = 0; l < 2048; ++l) {
-        bit_advantage[l][0] = -2;
+
+    for (int i = 2; i < BIT_ENCODE_SIZE; i++) {
+        bit_advantage[i][0] = -i;
     }
-    for (int k = 0; k < 2048; ++k) {
+
+    for (int bit_encode = 2; bit_encode < BIT_ENCODE_SIZE; bit_encode++)
         for (int i = 1; i < MAX_TRIGGER_SIZE; i++)
-            bit_advantage[k][i] = bit_advantage[k][i - 1] + ((i % (modulo + i - 1)) > 0) * 2;
-    }
+            bit_advantage[bit_encode][i] =
+                    bit_advantage[bit_encode][i - 1] + ((i % (int) (pow(2, bit_encode) - 1)) > 0) * 2
+                    + ((i % (int) (pow(2, bit_encode) - 1)) == 0) * (2 - bit_encode);
 
-    for (int k = 0; k < 2048; ++k) {
-        for (int j = 2; j < MAX_TRIGGER_SIZE; j++) {
-            for (int i = j; i < MAX_TRIGGER_SIZE; i++) {
-                trigger_size_advantage[k][j][BIT_ADVANTAGE] +=
-                        (occ_matr[0][i] + occ_matr[1][i] + occ_matr[2][i] + occ_matr[3][i])
-                        * bit_advantage[k][i - j];
-                trigger_size_advantage[k][j][FILE_SIZE] = total_byte / 4 - (trigger_size_advantage[k][j][0] / 4) +
-                                                          (((total_byte * 2 - trigger_size_advantage[k][j][0]) % 8) >
-                                                           0);
+    for (int bit_encode = 2; bit_encode < BIT_ENCODE_SIZE; bit_encode++)
+        for (int trigger_size = 1; trigger_size < MAX_TRIGGER_SIZE; trigger_size++)
+            for (int i = trigger_size; i < MAX_TRIGGER_SIZE; i++)
+                trigger_size_advantage[bit_encode][trigger_size] +=
+                        (occ_matr[POS('A')][i] + occ_matr[POS('C')][i] + occ_matr[POS('G')][i] + occ_matr[POS('T')][i])
+                        * bit_advantage[bit_encode][i - trigger_size];
+
+
+//    for(int i = 2; i<BIT_ENCODE_SIZE; i++)
+//        for (int j = 2; j < MAX_TRIGGER_SIZE; j++)
+//            printf("bit_encode: %d; trigger size: %d; trigg_size_adv: %lli\n", i, j, trigger_size_advantage[j][0]);
+
+
+//    for (int i=1; i<MAX_TRIGGER_SIZE; i++){
+//        printf("%d)\t%lli\t\t%lli\t\t%lli\t\t%lli\n", i, occ_matr[POS('A')][i], occ_matr[POS('C')][i], occ_matr[POS('G')][i], occ_matr[POS('T')][i]);
+//    }
+
+    int trigger_size_opt = 0;
+    int bit_encode_opt = 2;
+
+    for (int bit_encode = 2; bit_encode < BIT_ENCODE_SIZE; bit_encode++)
+        for (int trigger_size = 1; trigger_size < MAX_TRIGGER_SIZE; trigger_size++)
+            if (trigger_size_advantage[bit_encode][trigger_size] >
+                trigger_size_advantage[bit_encode_opt][trigger_size_opt]) {
+                trigger_size_opt = trigger_size;
+                bit_encode_opt = bit_encode;
             }
-        }
-    }
 
-    for (int k = 0; k < 2048; ++k) {
-        for (int i = 2; i < MAX_TRIGGER_SIZE; i++) {
-            printf("Opt trigger update %d %d\n", trigger_size_advantage[k][i][BIT_ADVANTAGE],
-                   trigger_size_advantage[k][opt_trigger_size][BIT_ADVANTAGE]);
-            if (trigger_size_advantage[k][i][BIT_ADVANTAGE] >
-                trigger_size_advantage[k][opt_trigger_size][BIT_ADVANTAGE]) {
-                *trigger_size = i;
-                *bit_encoding = k + 2;
-                printf("Opt trigger update %d\n", opt_trigger_size);
-            }
-        }
-    }
+    printf("trigger_size: %d, bit_encode_size: %d, adv_tot %lli\n", trigger_size_opt, bit_encode_opt, trigger_size_advantage[bit_encode_opt][trigger_size_opt]);
 
-    fseek(read_stream, 0, SEEK_SET);
-
-    printf("original size: %lu\n", total_byte);
-    printf("size with 2 bit encode: %lu\n", total_byte / 4);
-/*
-    for (int j = 2; j < MAX_TRIGGER_SIZE; j++)
-        printf("(%d) %d  %d\n", j, trigger_size_advantage[j][BIT_ADVANTAGE], trigger_size_advantage[j][FILE_SIZE]);
-
-    for (int i = 1; i < MAX_TRIGGER_SIZE; i++) {
-        printf("%d)\t%ld\t\t%ld\t\t%ld\t\t%ld\n", i, occ_matr[POS('A')][i], occ_matr[POS('C')][i],
-               occ_matr[POS('G')][i], occ_matr[POS('T')][i]);
-    }
-*/
-    return OSDNA_OK;
+            return OSDNA_OK;
 }
 
 void dump_occurence(char curr_char, int last_occ_len, osdna_bit_write_handler *bit_write_handle) {
